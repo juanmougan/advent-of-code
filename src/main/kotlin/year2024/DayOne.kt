0987c710b24e1ff3c1fc60eb7f2c1b8b0a9cd7de
Juan Manuel Mougan <juanmougan@gmail.com>
2024-12-02 11:12:37 -0300

    feat: second part of day 2

--- src/main/kotlin/year2024/DayOne.kt
+++ src/main/kotlin/year2024/DayOne.kt
@@ -13,3 +13,19 @@ fun solution(fileName: String, delimiter: String): Int {
 fun compareEachValue(firstListSorted: List<Int>, secondListSorted: List<Int>): List<Int> {
     return firstListSorted.indices.map { i -> abs(firstListSorted[i] - secondListSorted[i]) }.toList()
 }
+
+fun List<Int>.similarityScore(number: Int): Int {
+    // for 3 and [4, 3, 5, 3, 9, 3] it's 3 * 3 (there are three 3s on the list) = 9
+    return this.count { it == number } * number
+}
+
+fun List<Int>.similarityScore(other: List<Int>): Int {
+    return this.map { other.similarityScore(it) }.reduce { a, b -> a + b }
+}
+
+fun solutionSimilarityScore(fileName: String, delimiter: String): Int {
+    val bothLists = readLinesHavingTwoValues(fileName, delimiter)
+    val firstList = bothLists.map { it.first }.map { it.toInt() }
+    val secondList = bothLists.map { it.second }.map { it.toInt() }
+    return firstList.similarityScore(secondList)
+}
